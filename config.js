2bee1a77fab0ae1a6e9f30d0cac8e3cf5948a4ce
duchanhh <85352345+duchanhh@users.noreply.github.com>
2021-06-04 23:33:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn dĩ rất lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi vậy`,
+    introDesc: `trái đất vốn dĩ rất lạ thường
+    mà sao em cứ đi nhầm đường
+    lạc vào tim anh lẻ loi vậy`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên ',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: ' Phải chăng em đã yêu ngay từ cái nhìn đầu tiên ',
+    desc: ' Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh lắm <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
